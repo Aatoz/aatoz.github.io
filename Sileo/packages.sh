386f906f400261d9ba1fac9334d693469ede78ef
Noah Graydon <aatozb@gmail.com>
2019-05-14 01:08:41 -0400

    debs for SS and Fireflies

--- Sileo/packages.sh
+++ Sileo/packages.sh
@@ -2,3 +2,5 @@ dpkg-deb -Zgzip -b radial-widget
 dpkg-deb -Zgzip -b smooth-bar
 dpkg-deb -Zgzip -b bubble-mania
 dpkg-deb -Zgzip -b gradient-mania
+dpkg-deb -Zgzip -b solar-system
+dpkg-deb -Zgzip -b fireflies
